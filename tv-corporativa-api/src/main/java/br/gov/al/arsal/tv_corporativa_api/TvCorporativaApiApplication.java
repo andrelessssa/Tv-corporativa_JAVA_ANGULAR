44892c16eaf8d58ac91e0be15b1536ec0a992e01
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2026-03-29 12:08:34 -0300

    :tada:	init projeto criado
    :tada:	init projeto criado

---
+++ tv-corporativa-api/src/main/java/br/gov/al/arsal/tv_corporativa_api/TvCorporativaApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.al.arsal.tv_corporativa_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvCorporativaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvCorporativaApiApplication.class, args);
+	}
+
+}
---
+++ tv-corporativa-api/src/test/java/br/gov/al/arsal/tv_corporativa_api/TvCorporativaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.al.arsal.tv_corporativa_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvCorporativaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
