40004bb2d34605049b15b15da692f6deaab4d1e4
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2026-03-29 14:53:29 -0300

    :construction: criada a repository de midia
    :construction: criada a repository de midia

---
+++ tv-corporativa-api/src/main/java/br/gov/al/arsal/tv_corporativa_api/repository/MidiaRepository.java
@@ -0,0 +1,9 @@
+package br.gov.al.arsal.tv_corporativa_api.repository;
+
+import br.gov.al.arsal.tv_corporativa_api.model.Midia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MidiaRepository extends JpaRepository<Midia, Long> {
+
+    
+}
